e2fdf145395e42c67ce1b900a182e37596197041
getrom-raccoon <getromgleb@gmail.com>
2026-05-11 10:33:52 +0300

    add gmail

--- django_store/settings.py
+++ django_store/settings.py
@@ -115,3 +115,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Автоматическое поле по умолчанию
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'getromgleb@gmail.com'
+EMAIL_HOST_PASSWORD = 'onus ojfr nolq ojpk'
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
